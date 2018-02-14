32174ad86e1b35e2f6ade8bde4f3437ca80a16ce
wj107 <will.johnson@gmail.com>
2018-02-13 22:50:12 -0600

    v0.2: added option to time model creation

--- SEestimate.R
+++ SEestimate.R
@@ -1,16 +1,12 @@
 ###################################################################
-###################################################################
 #######estimating standard error of GLM/NN models via resampling
-###############
-#######but now looping both GLM and NN... not using cv.glm
-#######now use ALL of 'diamonds' to sample from, not just a subset
-#########################################################
-#########################################################
+###################################################################
 
 ####require ggplot, neuralnet, caTools, boot, plyr else stop
 ##pkgs<-c("ggplot2","neuralnet","caTools","boot","plyr")
 ##if(!(require(pkgs,quietly=T))) {stop}
 
+#---------------------FUNCTION CALL--------------------------------------
 
 ####define function to compare predictive analysis between GLM and NN:
 se.estimate<-function(
@@ -24,15 +20,17 @@ se.estimate<-function(
 		test.pct=0.10,		
 	#####size of test/validation sets in each re-sample groups. will override test.pct if given
 		test.size=NULL,
-	###percentage of re-sample data to be used as 'train' data set -- default 70%
-		##train.pct=0.70,
 	###select method(s) used to calculate standard error -- "glm", "nn", "both"
 		method="both",
 	###select type of response variable
 		distrib="continuous",
+	###record how long it takes to fit each model?
+		timer=F,
 	###progress bar?  T or F
 		prog.bar=T)
 	{
+
+#---------------------CHECK ARGUMENTS-------------------------
 
 ##########if no data set is given, default is 2000 row sample of 'diamonds' data set
 if(is.null(dat)){
@@ -44,14 +42,15 @@ if(is.null(dat)){
 #######check argument:  'data'
 if(!is.data.frame(dat)) stop("Argument `dat' must be a valid data frame")
 
-########a little more about our (now valid) data frame:
-	########how big is the data set??
-	N<-nrow(dat)
-	###get variable names
-	names(dat)->vars
+	#----------SOME DATA ATTRIBUTES------------
+		########how big is the data set??
+		N<-nrow(dat)
+		###get variable names
+		names(dat)->vars
 
 #######check argument:  'pred.var'
 if(!(pred.var %in% vars)) stop("Argument `pred.var' must be a column in the given data frame")
+
 #######check argument:  'resamples'
 if(!(resamples==round(resamples) && resamples>0)) stop("Argument `resamples' must be a positive integer")
 
@@ -66,134 +65,197 @@ if(!is.null(test.pct)) {
 	if(!(test.pct>0 && test.pct<=1)) stop("Argument `test.pct' must be a value greater than zero and less than or equal to 1")
 	test.size<-round(N*test.pct)
 	}
-#######check argument:  'train.pct'
-##if(!(train.pct>0 && train.pct<1)) stop("Argument `train.pct' must be a value between zero and one, exclusive")
+
 #######possible methods; make sure 'method' arg is valid
 method.options<-c("glm","nn","both")
 if(!(method %in% method.options)) stop("Argument `method' must be one of `glm', `nn', or `both'")
 #########check argument: 'distrib'
 if(!(distrib %in% c("continuous","categorical"))) stop("Argument `distrib' must be either `continuous' or `categorical'")
 
+#######check arugment: 'timer'
+if(!is.logical(timer)) stop("Argument `timer' must be logical")
 #######check argument: 'prog.bar'
 if(!is.logical(prog.bar)) stop("Argument `prog.bar' must be logical")
 
 
-##########prep data
+#-------------PREPARE DATA: formula, coerce numeric, scale data--------------
+
 	###identify predictive column in data frame
-	which(vars==pred.var)->resp.col
+		which(vars==pred.var)->resp.col
 	
 	#####create formula of from 'pred.var ~ .' for predictive model 
-	model.formula<-paste(vars[-resp.col],collapse=" + ")
-	model.formula<-paste(vars[resp.col],"~",model.formula)
-	model.formula<-as.formula(model.formula)
+		model.formula<-paste(vars[-resp.col],collapse=" + ")
+		model.formula<-paste(vars[resp.col],"~",model.formula)
+		model.formula<-as.formula(model.formula)
 
 	#######convert all data to numeric -- get rid of factors!!
-	dat2<-as.data.frame(lapply(dat,as.numeric))
+		dat2<-as.data.frame(lapply(dat,as.numeric))
 
 	#####unscale info for response
-	min(dat2[,resp.col])->resp.min	
-	max(dat2[,resp.col])->resp.max
+		min(dat2[,resp.col])->resp.min	
+		max(dat2[,resp.col])->resp.max
 	#####scale data
-	apply(dat2,2,min)->mins
-	apply(dat2,2,max)->maxs			
-	as.data.frame(scale(dat2,mins,maxs-mins))->dat2.scaled
+		apply(dat2,2,min)->mins
+		apply(dat2,2,max)->maxs			
+		as.data.frame(scale(dat2,mins,maxs-mins))->dat2.scaled
 
-################################
-###########GLM se.estimate
-################################
-	######check if glm is tested
+#-----------------ESTIMATE SE using GLM MODELS----------------------
+
+######check if glm is tested
 	if(method=="glm" || method=="both"){
-		####initialize output variable
+
+	#----------INITIALIZE OUTPUT, PROG BAR-------
+	
+	####initialize output variable(s)
 		glm.se.estimate<-NULL
-		#####if prog.bar=T, initialize progress bar
+		if(timer) glm.timer<-NULL
+	#####if prog.bar=T, initialize progress bar
 		if(prog.bar){
 			print("Re-sampling data, creating and testing models, to calculate standard error for a GLM model")
 			progress.bar<-create_progress_bar('text')
 			progress.bar$init(resamples)
 			}
-		#######train/test/compute SE  "resamples" number of times
+
+	#----------BEGIN RESAMPLING LOOP-----------------
+
+	###loop 'resamples' number of times
 		for(i in 1:resamples){
-			###find index for re-sampling TEST set
+
+		#----------DEFINE TRAIN/TEST SUBSETS----------
+
+		###find index for re-sampling TEST set
 			test.index<-sample(N,test.size)
-			###find its compliment -- the TRAIN set
+		###find its compliment -- the TRAIN set
 			train.index<-seq(1,N)[-test.index]
-			####define train/set from data
+		####define train/set from data
 			train<-dat[train.index,]
 			test<-dat[test.index,]
 		
-			####create glm!!
-				####for continuous response (assumed normal distrib)
-					if(distrib=="continuous") glm.model<-glm(model.formula,,train)
-				####for categorical response 
-					if(distrib=="categorical") glm.model<-glm(model.formula,family=binomial(link = 'logit'),data=train)
-			####predict from model:
-				###for continuous response
-					if(distrib=="continuous") pred.glm<-predict(glm.model,test)
-				###for categorical response
-					if(distrib=="categorical") pred.glm<-predict(glm.model,test,type="response")
-			####actual responses
+		#----------CREATE GLM MODEL!--------------
+
+		####for continuous response (assumed normal distrib)
+			if(distrib=="continuous"){
+				####if !timer
+					if(!timer) glm.model<-glm(model.formula,,train)
+				####if timer
+					if(timer) system.time(glm.model<-glm(model.formula,,train))[3]->glm.timer[i]
+				}
+	
+		####for categorical response 
+			if(distrib=="categorical"){
+				####if !timer
+					 if(!timer) glm.model<-glm(model.formula,family=binomial(link = 'logit'),data=train)
+				###if timer
+					 if(timer) system.time(glm.model<-glm(model.formula,family=binomial(link = 'logit'),data=train))[3]->glm.timer[i]
+				}
+
+		#----------PREDICT and TEST-------------
+
+		####predict from model:
+			###for continuous response
+				if(distrib=="continuous") pred.glm<-predict(glm.model,test)
+			###for categorical response
+				if(distrib=="categorical") pred.glm<-predict(glm.model,test,type="response")
+		
+		####actual responses
 			resp.glm<-test[,resp.col]
-			####calculate standard error
+		####calculate standard error
 			glm.se.estimate[i]<-sum((pred.glm-resp.glm)^2)/nrow(test)
-			####if prog.bar=T, step up
-			if(prog.bar) progress.bar$step()
-			}
-		##end glm estimate
-		}
+		
+	#--------END RESAMPLING LOOP-----------
+		if(prog.bar) progress.bar$step()
+		}	
 
+	###end glm.se.estimate
+	}
 
-###############################
-###########NN se.estimate
-###############################
-	######check if nn is tested
+#--------------------ESTIMATE SE using NN models--------------------------
+
+######check if nn is tested
 	if(method=="nn" || method=="both"){
-		####initialize output variable
+	
+	#---------INITIALIZE OUTPUT, PROG BAR-------
+
+	####initialize output variable(s)
 		nn.se.estimate<-NULL
-		#####if prog.bar=T, initialize progress bar
+		if(timer) nn.timer<-NULL
+	#####if prog.bar=T, initialize progress bar
 		if(prog.bar){
 			print("Re-sampling data, creating and testing models, to calculate standard error for a NN model")
 			progress.bar<-create_progress_bar('text')
 			progress.bar$init(resamples)
 			}
-		#######train/test/compute SE  "resamples" number of times
+
+	#---------BEGIN RESAMPLE LOOP----------
+	
+	###loop "resamples" number of times
 		for(i in 1:resamples){
-			###find index for re-sampling TEST set
+	
+		#--------DEFINE TRAIN/TEST SUBSETS--------
+			
+		###find index for re-sampling TEST set
 			test.index<-sample(N,test.size)
-			###find its compliment -- the TRAIN set
+		###find its compliment -- the TRAIN set
 			train.index<-seq(1,N)[-test.index]
-			####define train/set from data... use dat2.scaled for NN!
+		####define train/set from data... use dat2.scaled for NN!
 			train<-dat2.scaled[train.index,]
 			test<-dat2.scaled[test.index,]
 
-			####create neuralnet!!
-			nn<-neuralnet(model.formula,train,hidden=c(10,10,10),linear.output=F)
-			####predict, and re-scale:
+		#---------CREATE NEURAL NET!!----------
+
+		####if !timer
+			if(!timer) nn<-neuralnet(model.formula,train,hidden=c(10,10,10),linear.output=F)
+		####if timer
+			if(timer) system.time(nn<-neuralnet(model.formula,train,hidden=c(10,10,10),linear.output=F))[3]->nn.timer[i]
+		
+		#----------PREDICT AND TEST----------
+			
+		####predict, and re-scale:
 			pred.nn<-compute(nn,test[,-resp.col])
 			pred.nn<-pred.nn$net.result*(resp.max-resp.min)+resp.min
-			####actual responses
+		####actual responses
 			resp.nn<-test[,resp.col]*(resp.max-resp.min)+resp.min
-			##resp.nn<-dat[test.index,resp.col]  ###does it work too??
-			####calculate standard error
+		
+		##resp.nn<-dat[test.index,resp.col]  ###does it work too??
+		####calculate standard error
 			nn.se.estimate[i]<-sum((resp.nn-pred.nn)^2)/nrow(test)
-			####if prog.bar=T, step up
-			if(prog.bar) progress.bar$step()
-			}
-		###end nn estimates
+			
+
+	#----------END RESAMPLING LOOP----------
+		if(prog.bar) progress.bar$step()
 		}
 
+	###end nn estimates
+	}
 
-#####################
-###output
-#######################
 
-######if method=glm
-	if(method=="glm") se.results<-list(GLM.se.estimate=glm.se.estimate,GLM.summary=summary(glm.se.estimate)) 
+#-------------------OUTPUT RESULTS------------------
 
-######if method=glm
-	if(method=="nn") se.results<-list(NN.se.estimate=nn.se.estimate,NN.summary=summary(nn.se.estimate)) 
+	######if method=glm
+		if(method=="glm"){
+			if(!timer) se.results<-list(GLM.se.estimate=glm.se.estimate,GLM.summary=summary(glm.se.estimate)) 
+			if(timer) se.results<-list(GLM.se.estimate=glm.se.estimate,GLM.summary=summary(glm.se.estimate),GLM.timer=glm.timer)
+			} 
 
-######if method=glm
-	if(method=="both") se.results<-list(GLM.se.estimate=glm.se.estimate,NN.se.estimate=nn.se.estimate,GLM.summary=summary(glm.se.estimate),NN.summary=summary(nn.se.estimate)) 
+	######if method=glm
+		if(method=="nn"){
+			if(!timer) se.results<-list(NN.se.estimate=nn.se.estimate,NN.summary=summary(nn.se.estimate))
+			if(timer) se.results<-list(NN.se.estimate=nn.se.estimate,NN.summary=summary(nn.se.estimate),NN.timer=nn.timer) 
+			}
 
-####output results
-se.results}
+	######if method=glm
+		if(method=="both"){
+			if(!timer){
+				se.results<-list(GLM.se.estimate=glm.se.estimate,NN.se.estimate=nn.se.estimate,
+				GLM.summary=summary(glm.se.estimate),NN.summary=summary(nn.se.estimate))
+				} 
+			if(timer){
+				se.results<-list(GLM.se.estimate=glm.se.estimate,NN.se.estimate=nn.se.estimate,
+				GLM.summary=summary(glm.se.estimate),NN.summary=summary(nn.se.estimate),
+				GLM.timer=glm.timer,NN.timer=nn.timer)
+				}	
+			}	
+		
+#------OUTPUT!--------
+se.results
+}
